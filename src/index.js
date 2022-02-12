b8f7e21f19e840c15c7d7f3210cc7d288e365962
JohnPosada <jposadaz@unal.edu.co>
2022-02-12 15:08:52 -0500

    Added parcel configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./main.css";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const app = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, 
+  app
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
