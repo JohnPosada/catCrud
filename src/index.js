ac4e7536b802b5b26ed01ed31bb9292ce497e858
JohnPosada <jposadaz@unal.edu.co>
2022-02-12 23:50:26 -0500

    added routes for home, create and delete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./main.css";
-import App from './App';
+import App from "./App";
+import Create from "./pages/create";
+import Delete from "./pages/delete";
 
 const app = document.getElementById("root");
 ReactDOM.render(
-  <App />, 
+  <BrowserRouter>
+    <Routes>
+      <Route exact strict path="/" element={<App />} />
+      <Route exact strict path="/create" element={<Create />} />
+      <Route exact strict path="/delete" element={<Delete />} />
+    </Routes>
+  </BrowserRouter>,
+
   app
 );
-
-
