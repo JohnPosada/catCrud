1888e316f7ae76b9e0f0feba736cb1012b041175
JohnPosada <jposadaz@unal.edu.co>
2022-02-14 00:10:56 -0500

    added background image and operaror ternary to create page

--- src/pages/create.js
+++ src/pages/create.js
@@ -44,7 +44,7 @@ const createVote = async (body) => {
 };
 
 const Create = () => {
-  const [vote, setVote] = useState({});
+  const [vote, setVote] = useState(null);
   const handleCreate = async (event) => {
     event.preventDefault();
     const image_id = event.target.elements.image_id.value;
@@ -61,58 +61,69 @@ const Create = () => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex h-screen flex-col bg-opacity-80 bg-cat-pattern bg-[length:100px_100px] bg-clip-content bg-repeat">
       <Navbar />
-      <form
-        className="mx-auto mt-10 flex w-96 flex-col justify-center rounded-3xl bg-sky-700 p-10"
-        onSubmit={handleCreate}
-      >
-        <h2 className="mb-2 font-extrabold text-white">Create a new vote </h2>
-        <label
-          className="pl-15 m-2 flex flex-col text-white"
-          htmlFor="image_id"
+      <div className="flex h-screen items-center justify-center">
+        <form
+          className="mx-auto mt-10 flex w-96 flex-col justify-center rounded-3xl bg-sky-700 p-10"
+          onSubmit={handleCreate}
         >
-          <span className="mb-2 font-bold">image_id</span>
-          <input
-            className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
-            type="text"
-            name="image_id"
-            id="image_id"
-          />
-        </label>
+          <h2 className="mb-2 font-extrabold text-white">Create a new vote </h2>
+          <label
+            className="pl-15 m-2 flex flex-col text-white"
+            htmlFor="image_id"
+          >
+            <span className="mb-2 font-bold">image_id</span>
+            <input
+              className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
+              type="text"
+              name="image_id"
+              id="image_id"
+            />
+          </label>
 
-        <label className="pl-15 m-2 flex flex-col text-white" htmlFor="sub_id">
-          <span className="mb-2 font-bold">sub_id</span>
-          <input
-            className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
-            type="text"
-            name="sub_id"
-            id="sub_id"
-          />
-        </label>
+          <label
+            className="pl-15 m-2 flex flex-col text-white"
+            htmlFor="sub_id"
+          >
+            <span className="mb-2 font-bold">sub_id</span>
+            <input
+              className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
+              type="text"
+              name="sub_id"
+              id="sub_id"
+            />
+          </label>
 
-        <label
-          className="pl-15 m-2 flex flex-col text-white"
-          htmlFor="voteValue"
-        >
-          <span className="mb-2 font-bold">Vote Value</span>
-          <input
-            className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
-            type="number"
-            name="voteValue"
-            id="voteValue"
-          />
-        </label>
+          <label
+            className="pl-15 m-2 flex flex-col text-white"
+            htmlFor="voteValue"
+          >
+            <span className="mb-2 font-bold">Vote Value</span>
+            <input
+              className="w-full appearance-none rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
+              type="number"
+              name="voteValue"
+              id="voteValue"
+              max="1"
+              min="0"
+            />
+          </label>
 
-        <input
-          className="mt-6 cursor-pointer rounded-lg bg-gray-700 p-2 font-bold text-white hover:bg-gray-300 hover:text-gray-900"
-          type="submit"
-          name="submit"
-          id="submit"
-          value="Create"
-        />
-      </form>
-      <div>{vote.vote_id}</div>
+          <input
+            className="mt-6 cursor-pointer rounded-lg bg-gray-700 p-2 font-bold text-white hover:bg-gray-300 hover:text-gray-900"
+            type="submit"
+            name="submit"
+            id="submit"
+            value="Create"
+          />
+          {vote !== null ? (
+            <h2 name="voteId" className="mt-2 font-bold text-white">
+              Vote Id: {vote.vote_id}
+            </h2>
+          ) : null}
+        </form>
+      </div>
     </div>
   );
 };
