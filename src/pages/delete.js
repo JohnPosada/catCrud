f12823a4f85ecf0f9f8ffe54eb36378a28a214a3
JohnPosada <jposadaz@unal.edu.co>
2022-02-14 00:12:17 -0500

    added background image to delete page

--- src/pages/delete.js
+++ src/pages/delete.js
@@ -44,7 +44,7 @@ const deleteVote = async (vote_id) => {
 
 const Delete = () => {
   const [deletedVote, setDeletedVote] = useState({});
-  const [votes, setVotes] = useState([]);
+  const [votes, setVotes] = useState([{ id: "no items" }]);
   const [selectedVote, setSelectedVote] = useState("");
 
   const handleDelete = async () => {
@@ -56,43 +56,49 @@ const Delete = () => {
     setSelectedVote(event.target.value);
   };
 
-  useEffect(async () => {
+  const handleClickVote = async (event) => {
     const votes = await getVotes();
     setVotes(votes);
-  }, []);
+  };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex h-screen flex-col bg-opacity-80 bg-cat-pattern bg-[length:100px_100px] bg-clip-content bg-repeat">
       <Navbar />
-      <form
-        className="mx-auto mt-10 flex w-96 flex-col justify-center rounded-3xl bg-sky-700 p-10"
-        onSubmit={handleDelete}
-      >
-        <label className="pl-15 m-2 flex flex-col text-white" htmlFor="select">
-          <span className="mb-2 font-bold">Select a vote to delete it</span>
-          <select
-            className="w-full rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
-            name="select"
-            id="select"
-            onChange={handleSelectVote}
+      <div className="flex h-screen items-center justify-center">
+        <form
+          className="mx-auto mt-10 flex w-96 flex-col justify-center rounded-3xl bg-sky-700 p-10"
+          onSubmit={handleDelete}
+        >
+          <label
+            className="pl-15 m-2 flex flex-col text-white"
+            htmlFor="select"
           >
-            {votes.map(({ id }) => {
-              return (
-                <option key={id} value={id}>
-                  {id}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <input
-          className="mt-6 cursor-pointer rounded-lg bg-gray-700 p-2 font-bold text-white hover:bg-gray-300 hover:text-gray-900"
-          type="submit"
-          name="submit"
-          id="submit"
-          value="Delete"
-        />
-      </form>
+            <span className="mb-2 font-bold">Select a vote to delete it</span>
+            <select
+              className="w-full rounded border-2 border-black bg-gray-200 py-2 px-4 leading-tight text-gray-700 focus:border-gray-200 focus:bg-white focus:outline-none"
+              name="select"
+              id="select"
+              onChange={handleSelectVote}
+              onClick={handleClickVote}
+            >
+              {votes.map(({ id }) => {
+                return (
+                  <option key={id} value={id}>
+                    {id}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <input
+            className="mt-6 cursor-pointer rounded-lg bg-gray-700 p-2 font-bold text-white hover:bg-gray-300 hover:text-gray-900"
+            type="submit"
+            name="submit"
+            id="submit"
+            value="Delete"
+          />
+        </form>
+      </div>
     </div>
   );
 };
